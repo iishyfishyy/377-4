bae9d0b7ccc6c4cbb224de8131d56857a0a54dca
JoseAlba <josealba1996@hotmail.com>
2017-10-24 23:45:44 -0400

    Damage excluding Boss

--- Assets/characters/Rogue.cs
+++ Assets/characters/Rogue.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rogue : MonoBehaviour {
+public class Rogue : Character{
 
 	// Use this for initialization
 	void Start () {
-		
+		this.health = 3000;
+		this.mana = 0;
+		this.damage = this.damage_dealt ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		this.damage = this.damage_dealt ();	
 	}
+
+	protected override int damage_dealt (){
+		return this.GetRandomNumber (15,20);
+	}
+
+
 }
--- Assets/characters/Warrior.cs
+++ Assets/characters/Warrior.cs
@@ -6,11 +6,20 @@ public class Warrior : Character {
 
 	// Use this for initialization
 	void Start () {
-		
+		this.health = 3000;
+		this.mana = 0;
+		this.damage = this.damage_dealt ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	protected override int damage_dealt (){
+		return 5;
+	}
+
+
+	
 }
